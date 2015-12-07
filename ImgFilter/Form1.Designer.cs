c56364d5f0ba2b06594d7d0c5d46fdd0c3e19af1
aaaddress1 <aaaddress1@gmail.com>
2015-12-07 09:05:59 +0800

    no message

---
+++ ImgFilter/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace ImgFilter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.demoPictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.demoPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox.Image")));
+            this.pictureBox.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(258, 258);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(301, 276);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 31);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Median Filter";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(423, 276);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(125, 31);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Mean Filter";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // demoPictureBox
+            // 
+            this.demoPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.demoPictureBox.Location = new System.Drawing.Point(289, 12);
+            this.demoPictureBox.Name = "demoPictureBox";
+            this.demoPictureBox.Size = new System.Drawing.Size(258, 258);
+            this.demoPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.demoPictureBox.TabIndex = 3;
+            this.demoPictureBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(560, 312);
+            this.Controls.Add(this.demoPictureBox);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "Form1";
+            this.Text = "Image Filter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.demoPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox demoPictureBox;
+    }
+}
+
---
+++ ImgFilter/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImgFilter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Bitmap orgPic = new Bitmap(this.pictureBox.Image);
+            Bitmap tmpPic = new Bitmap(this.pictureBox.Image);
+            for (int y = 1; y < orgPic.Height - 1; y++)
+            {
+                for (int x = 1; x < orgPic.Width - 1; x++)
+                {
+                    List<int> list = new List<int>();
+                    list.Add(orgPic.GetPixel(x - 1, y - 1).ToArgb());
+                    list.Add(orgPic.GetPixel(x - 0, y - 1).ToArgb());
+                    list.Add(orgPic.GetPixel(x + 1, y - 1).ToArgb());
+                    list.Add(orgPic.GetPixel(x - 1, y - 0).ToArgb());
+                    list.Add(orgPic.GetPixel(x - 0, y - 0).ToArgb());
+                    list.Add(orgPic.GetPixel(x + 1, y - 0).ToArgb());
+                    list.Add(orgPic.GetPixel(x - 1, y + 1).ToArgb());
+                    list.Add(orgPic.GetPixel(x - 0, y + 1).ToArgb());
+                    list.Add(orgPic.GetPixel(x + 1, y + 1).ToArgb());
+                    list.Sort();
+                    tmpPic.SetPixel(x, y, Color.FromArgb(list[5]));
+                }
+            }
+            demoPictureBox.Image = tmpPic;
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Bitmap orgPic = new Bitmap(this.pictureBox.Image);
+            Bitmap tmpPic = new Bitmap(this.pictureBox.Image);
+            for (int y = 1; y < orgPic.Height - 1; y++)
+            {
+                for (int x = 1; x < orgPic.Width - 1; x++)
+                {
+                    Point[] arrayP = new Point[] {  new Point(x - 1, y - 1),
+                                                    new Point(x - 0, y - 1),
+                                                    new Point(x + 1, y - 1),
+                                                    new Point(x - 1, y - 0),
+                                                    new Point(x - 0, y - 0),
+                                                    new Point(x + 1, y - 0),
+                                                    new Point(x - 1, y + 1),
+                                                    new Point(x - 0, y + 1),
+                                                    new Point(x + 1, y + 1)};
+
+                    int sumA = 0 , sumR = 0, sumG = 0, sumB = 0;
+                    foreach (Point item in arrayP)
+                    {
+                        sumA += orgPic.GetPixel(item.X, item.Y).A;
+                        sumR += orgPic.GetPixel(item.X, item.Y).R;
+                        sumG += orgPic.GetPixel(item.X, item.Y).G;
+                        sumB += orgPic.GetPixel(item.X, item.Y).B;
+                    }
+                    int meanARGB = 0x01000000 * (sumA/9) +
+                                   0x00010000 * (sumR/9) +
+                                   0x00000100 * (sumG/9) +
+                                   0x00000001 * (sumB/9) ;
+
+                    tmpPic.SetPixel(x, y, Color.FromArgb(meanARGB));
+                }
+            }
+            demoPictureBox.Image = tmpPic;
+        }
+    }
+}
